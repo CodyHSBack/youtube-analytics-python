4872379586516adfed1fe831326398daa421da36
Cody Back <codyback1996@gmail.com>
2021-04-22 23:50:27 -0400

    FEAT: Channel ID is set as input
    Made it to where the channel ID can be set as input when main.py is ran.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from youtube_statistics import YTstats
 
 API_KEY = "AIzaSyCIR_ILZDNu-Yb8wj6i28ILJS2kQNL9qbk"
-channel_id = "UCnZ1r94_Ptz_1gN5VBnE0Mg"
+
+channel_id = input("Enter channel ID: ")
 
 yt = YTstats(API_KEY, channel_id)
 data = yt.get_channel_statistics()
